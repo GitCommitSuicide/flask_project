cb66c756c0bf409257dfb103e622ecca1ebcced9
Anil <anilramawat190@gmail.com>
2026-02-18 17:02:44 +0530

    Add Procfile for deployment

--- app.py
+++ app.py
@@ -314,5 +314,7 @@ def progress_data():
   db_session.close()
   return jsonify(data)
 
-if __name__ == '__main__':
-  app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
